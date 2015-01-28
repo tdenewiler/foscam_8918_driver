deac4e2b5a94b4b1e8b000cf321b786556833dbc
Thomas Denewiler <tdenewiler@gmail.com>
2015-01-27 19:38:37 -0800

    #1: Fixing spacing per roscpp style guide.

--- src/foscam_8918_driver.cpp
+++ src/foscam_8918_driver.cpp
@@ -8,33 +8,33 @@ Foscam8918::Foscam8918(ros::NodeHandle nh_) :
     camera_pub_(it_->advertiseCamera("image_raw", 1)),
     camera_info_manager_(new camera_info_manager::CameraInfoManager(nh_))
 {
-    // Set up a dynamic reconfigure server.
-    reconfig_cb = boost::bind(&Foscam8918::configCallback, this, _1, _2);
-    reconfig_srv.setCallback(reconfig_cb);
+  // Set up a dynamic reconfigure server.
+  reconfig_cb = boost::bind(&Foscam8918::configCallback, this, _1, _2);
+  reconfig_srv.setCallback(reconfig_cb);
 
-    // Initialize node parameters.
-    ros::NodeHandle pnh("~");
-    pnh.param("username",   username_,   std::string("change_me"));
-    pnh.param("password",   password_,   std::string("change_me"));
-    pnh.param("ip_address", ip_address_, std::string("192.168.1.1"));
-    pnh.param("port",       port_,       std::string("80"));
-    pnh.param("url_suffix", url_suffix_, std::string("video.cgi?.mjpg"));
-    pnh.param("url_prefix", url_prefix_, std::string("http://"));
-    pnh.param("frame_id",   frame_id_,   std::string("foscam"));
-    pnh.param("rate",       rate_,       int(10));
-    if (rate_ <= 0)
-    {
-        rate_ = 1;
-    }
+  // Initialize node parameters.
+  ros::NodeHandle pnh("~");
+  pnh.param("username",   username_,   std::string("change_me"));
+  pnh.param("password",   password_,   std::string("change_me"));
+  pnh.param("ip_address", ip_address_, std::string("192.168.1.1"));
+  pnh.param("port",       port_,       std::string("80"));
+  pnh.param("url_suffix", url_suffix_, std::string("video.cgi?.mjpg"));
+  pnh.param("url_prefix", url_prefix_, std::string("http://"));
+  pnh.param("frame_id",   frame_id_,   std::string("foscam"));
+  pnh.param("rate",       rate_,       int(10));
+  if (rate_ <= 0)
+  {
+    rate_ = 1;
+  }
 
-    // Create a timer callback.
-    ros::Timer timer = nh_.createTimer(ros::Duration(1.0/rate_), &Foscam8918::timerCallback, this);
+  // Create a timer callback.
+  ros::Timer timer = nh_.createTimer(ros::Duration(1.0/rate_), &Foscam8918::timerCallback, this);
 
-    // Connect to the camera.
-    connectToCamera();
+  // Connect to the camera.
+  connectToCamera();
 
-    // Let callbacks take over.
-    ros::spin();
+  // Let callbacks take over.
+  ros::spin();
 }
 
 Foscam8918::~Foscam8918()
@@ -43,60 +43,60 @@ Foscam8918::~Foscam8918()
 
 bool Foscam8918::connectToCamera()
 {
-    // URL of camera video stream.
-    const std::string video_stream_address = url_prefix_ + username_ + ":" + password_ + "@" + ip_address_ + ":" + port_ + "/" + url_suffix_;
+  // URL of camera video stream.
+  const std::string video_stream_address = url_prefix_ + username_ + ":" + password_ + "@" + ip_address_ + ":" + port_ + "/" + url_suffix_;
 
-    // Open the video stream and make sure it's opened.
-    have_connection_ = true;
-    if (!vcap_.open(video_stream_address))
-    {
-        have_connection_ = false;
-        ROS_ERROR("Error opening video stream or file");
-        return false;
-    }
+  // Open the video stream and make sure it's opened.
+  have_connection_ = true;
+  if (!vcap_.open(video_stream_address))
+  {
+    have_connection_ = false;
+    ROS_ERROR("Error opening video stream or file");
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 void Foscam8918::timerCallback(const ros::TimerEvent& event)
 {
-    cv::Mat image;
+  cv::Mat image;
 
-    if (have_connection_)
+  if (have_connection_)
+  {
+    if (!vcap_.read(image))
     {
-        if (!vcap_.read(image))
-        {
-            ROS_WARN("No frame");
-            cv::waitKey();
-        }
-        //! \todo Clean up the header values with use of parameters and better spot for getting time.
-        cv_img_.header.stamp = ros::Time::now();
-        cv_img_.header.frame_id = frame_id_;
-        cv_img_.encoding = "bgr8";
-        cv_img_.image = image;
-
-        sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(camera_info_manager_->getCameraInfo()));
-        ci->height = image.rows;
-        ci->width = image.cols;
-        ci->header = cv_img_.header;
-
-        camera_pub_.publish(cv_img_.toImageMsg(), ci);
+      ROS_WARN("No frame");
+      cv::waitKey();
     }
+    //! \todo Clean up the header values with use of parameters and better spot for getting time.
+    cv_img_.header.stamp = ros::Time::now();
+    cv_img_.header.frame_id = frame_id_;
+    cv_img_.encoding = "bgr8";
+    cv_img_.image = image;
+
+    sensor_msgs::CameraInfoPtr ci(new sensor_msgs::CameraInfo(camera_info_manager_->getCameraInfo()));
+    ci->height = image.rows;
+    ci->width = image.cols;
+    ci->header = cv_img_.header;
+
+    camera_pub_.publish(cv_img_.toImageMsg(), ci);
+  }
 }
 
 void Foscam8918::configCallback(foscam_8918_driver::foscam_8918_driverConfig &config, uint32_t level)
 {
-    username_ = config.username;
-    password_ = config.password;
-    ip_address_ = config.ip_address;
-    port_ = config.port;
-    url_suffix_ = config.url_suffix;
+  username_ = config.username;
+  password_ = config.password;
+  ip_address_ = config.ip_address;
+  port_ = config.port;
+  url_suffix_ = config.url_suffix;
 
-    if (config.reset_connection)
-    {
-        config.reset_connection = false;
-        connectToCamera();
-        ROS_INFO("Resetting connection to camera with current parameters.");
-    }
+  if (config.reset_connection)
+  {
+    config.reset_connection = false;
+    connectToCamera();
+    ROS_INFO("Resetting connection to camera with current parameters.");
+  }
 }
 }
--- src/nodes/foscam_8918_driver_node.cpp
+++ src/nodes/foscam_8918_driver_node.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char **argv)
 {
-    // Set up ROS.
-    ros::init(argc, argv, "foscam_8918_driver");
-    ros::NodeHandle nh;
+  // Set up ROS.
+  ros::init(argc, argv, "foscam_8918_driver");
+  ros::NodeHandle nh;
 
-    Foscam8918Driver::Foscam8918 foscam_8918(nh);
+  Foscam8918Driver::Foscam8918 foscam_8918(nh);
 
-    return 0;
+  return 0;
 }
